a3df7a8b870133de03296eab1208b13ad9f884df
kimyongkuk <k0603156@gmail.com>
2020-02-03 06:21:42 +0900

    .

---
+++ travelers-ui/dist/types/DatePicker/datePicker.d.ts
@@ -0,0 +1,3 @@
+import { DatePickerProps } from "antd/lib/date-picker/interface";
+declare const datePicker: import("styled-components").StyledComponent<import("antd/es/date-picker/interface").DatePickerDecorator, any, DatePickerProps, never>;
+export default datePicker;
---
+++ travelers-ui/dist/types/Input/input.d.ts
@@ -0,0 +1,3 @@
+import Input, { InputProps } from "antd/es/input";
+declare const input: import("styled-components").StyledComponent<typeof Input, any, InputProps, never>;
+export default input;
